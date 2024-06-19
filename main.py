be91057928e6ab7fcc091e3ea855154a63ca6b0f
FrancescoCesari97 <freedus97@gmail.com>
2024-06-19 11:06:53 +0200

    added rock paper scissors game

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import random
 
 # print(help(random))
 
-low = 1
+# low = 1
 
-high = 100
+# high = 100
 
-guesses = 0
+# guesses = 0
 
-number = random.randint(low, high)
+# number = random.randint(low, high)
 
 
 # options = ("rock", "paper", "scissors")
@@ -28,16 +28,50 @@ number = random.randint(low, high)
 # print(number)
 
 
-while True:
-    guess = int(input(f"Enter a number between {low} and {high} "))
-    guesses += 1
+# while True:
+#     guess = int(input(f"Enter a number between {low} and {high} "))
+#     guesses += 1
 
-    if guess < number:
-        print(f"{guess} is too low")
-    elif guess > number:
-        print(f"{guess} is too high")
-    else:
-        print("you guessed right ")
-        break
+#     if guess < number:
+#         print(f"{guess} is too low")
+#     elif guess > number:
+#         print(f"{guess} is too high")
+#     else:
+#         print("you guessed right ")
+#         break
 
-print(f"number of guesses: {guesses}")
+# print(f"number of guesses: {guesses}")
+
+
+##### ROCK PAPER SCISSORS
+
+
+options = ("rock", "paper", "scissors")
+
+player = None 
+
+computer = random.choice(options)
+
+player = input("Enter a choice: ").lower()
+
+while player not in options:
+    print("Enter a valid choice (rock, paper, scissors)")
+    player = input("Enter a choice: ").lower()
+
+print(f"Player chose: {player}")
+print(f"Computer chose: {computer}")
+
+
+
+if player == computer:
+    print("it's a tie!")
+elif player == "rock" and computer == "scissors":
+    print ("player win!")
+elif player == "paper" and computer == "rock":
+    print("player win!")
+elif player == "scissors" and computer == "paper":
+    print("player win")
+else:
+    print("computer won!")
+   
+    
